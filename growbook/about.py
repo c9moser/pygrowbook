b6e48a739ca8cada0f96d480d4edb8bbf5cef995
Christian Moser <c9moser@gmail.com>
2020-09-26 21:17:53 +0200

    0.0.20

--- growbook/about.py
+++ growbook/about.py
@@ -1,9 +1,30 @@
+# growbook/about.py
+################################################################################
+# Copyright (C) 2020  Christian Moser                                          #
+#                                                                              #
+#   This program is free software: you can redistribute it and/or modify       #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+################################################################################
+
 import gi
 from gi.repository import Gtk
 import os
 import config
 
-(ABOUT_UI,)=(os.path.join(os.path.dirname(__file__),"about.ui"),)
+(   
+    ABOUT_UI,
+    LICENSE_FILE
+)=(
+    os.path.join(os.path.dirname(__file__),"about.ui"),
+    os.path.join(os.path.dirname(__file__),"license.txt")
+)
 
 class AboutDialogHandle(object):
     def __init__(self,parent):
@@ -14,7 +35,9 @@ class AboutDialogHandle(object):
         self.dialog=builder.get_object("aboutdialog1")
         self.dialog.set_transient_for(parent)
         self.dialog.handler=self
-
+        
+        #with open(LICENSE_FILE,'r') as f:
+            #self.dialog.set_license(f.read())
         self.dialog.set_version('.'.join((str(i) for i in config.config['version'])))
         self.dialog.show_all()
         
